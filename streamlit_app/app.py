3017e2318eb382434a95af446eb597d291accb33
basamba1990 <basamba1990@yahoo.fr>
2025-05-02 20:01:20 +0000

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -6,7 +6,7 @@ from whisper_utils import transcribe_audio
 st.set_page_config(page_title="Pitch Uploader - GENUP2050", layout="centered")
 st.title("Pitch Uploader - GENUP2050")
 
-video_file = st.file_uploader("Téléverse ta vidéo de pitch", type=["mp4", "mov", "m4a", "mp3"])
+video_file = st.file_uploader("Téléverse ta vidéo de pitch", type=["mp4", "mov", "m4a", "wav", "mp3"])
 
 if video_file is not None:
     with tempfile.NamedTemporaryFile(delete=False, suffix=video_file.name) as temp_file:
@@ -19,7 +19,7 @@ if video_file is not None:
         transcription = transcribe_audio(temp_file_path)
         st.text_area("Transcription :", transcription, height=200)
 
-        bucket_name = "pitch-videos"
+        bucket_name = "videos"
 
         # Upload de la vidéo
         supabase.storage.from_(bucket_name).upload(video_file.name, temp_file_path)
